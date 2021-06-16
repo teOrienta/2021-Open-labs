412dc7d9e0bc95c9f6de274ef9c722373878b664
Danilo Silva <jdsc@cin.ufpe.br>
2021-06-16 19:31:25 -0300

    Login comopnent

--- nextjs/components/Header.tsx
+++ nextjs/components/Header.tsx
@@ -2,9 +2,37 @@ import Image from 'next/image';
 import Link from 'next/link';
 import styles from '../styles/components/Header.module.css'
 import Icon from '../public/icons/vercel.svg';
+import { useState } from 'react';
+
+export function LoginContainer() {
+  return (
+    <div className = {styles.loginContainer}>
+      <h1> Entrar </h1>
+      <input type="email" placeholder="E-mail" required/>
+      <input type="password" placeholder="Senha" required/>
+      <button type = "button" className = {styles.access}
+          onClick = {() => {}}>
+          Entrar
+      </button>
+      <div className = {styles.separator}>
+          <div> 
+              <span>
+                  Ou se cadastre aqui
+              </span>
+          </div>
+      </div>
+      <a className = {styles.buy}> 
+          Cadastrar 
+      </a>
+    </div>
+  )
+}
 
 export default function Header() {
+  const [showLogin, setShowLogin] = useState(false);
+
   return (
+    <>
     <div className = {styles.container}>
       <Image src = {Icon} alt = "Program icon" />
 
@@ -12,10 +40,20 @@ export default function Header() {
           <Link href = "/">
             <a> Sobre nós </a>
           </Link>
-          <button className = "colored-btn">
+          <button className = "colored-btn"
+            onClick = {() => {setShowLogin(!showLogin)}}>
               Entrar
           </button>
       </div>
     </div>
+    {showLogin ? 
+      <>
+        <LoginContainer/>
+        <div className = {styles.overlay}
+          onClick = {() => {setShowLogin(!showLogin)}}
+        ></div>
+      </>
+    :<> </>}
+    </>
   )
 }
