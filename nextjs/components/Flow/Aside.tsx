cf0afda8f9c4c78e1033d8d38001472ff5d93d60
Danilo Silva <jdsc@cin.ufpe.br>
2021-06-17 09:13:38 -0300

    flow responsive

--- nextjs/components/Flow/Aside.tsx
+++ nextjs/components/Flow/Aside.tsx
@@ -2,14 +2,29 @@ import Image from 'next/image';
 import styles from '../../styles/components/Flow/Aside.module.css'
 import Icon from '../../public/icons/teorienta.png';
 
-import { AiFillHome, AiOutlineUpload, AiFillFilter } from 'react-icons/ai';
+import { AiFillHome, AiOutlineUpload, AiFillFilter, 
+  AiFillCaretLeft, AiFillCaretRight } from 'react-icons/ai';
+import { useState } from 'react';
 
 export default function Aside() {
+  const [showAside, setShowAside] = useState("translateX(0px)");
 
   return (
-    <div className = {styles.container}>
-      <div className = {styles.logoContainer}>
-        <Image src = {Icon} alt = "Program icon" />
+    <>
+    {showAside === "translateX(-240px)" ? 
+      <button onClick = {() => {setShowAside("translateX(0px)")}}
+        className = {styles.showBtn}>
+          <AiFillCaretRight/>
+      </button>
+    : <></>}
+    <div className = {styles.container} style = {{ transform: showAside }}>
+      <div className = {styles.title}>
+        <div className = {styles.logoContainer}>
+          <Image src = {Icon} alt = "Program icon" />
+        </div>
+        <button onClick = {() => {setShowAside("translateX(-240px)")}}>
+          <AiFillCaretLeft/>
+        </button>
       </div>
 
       <button className = {styles.selected}>
@@ -24,5 +39,6 @@ export default function Aside() {
         <AiFillFilter/> Filtragem
       </button>
     </div>
+    </>
   )
 }
