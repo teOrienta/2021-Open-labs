a4a38c1000431288908e2fb0639ad47a40cb5cb5
Danilo Silva <jdsc@cin.ufpe.br>
2021-06-16 21:23:00 -0300

    aside component

--- nextjs/components/Flow/Aside.tsx
+++ nextjs/components/Flow/Aside.tsx
@@ -1,22 +1,28 @@
 import Image from 'next/image';
-import Link from 'next/link';
-import styles from '../../styles/components/Header.module.css'
+import styles from '../../styles/components/Flow/Aside.module.css'
 import Icon from '../../public/icons/vercel.svg';
+
+import { AiFillHome, AiOutlineUpload, AiFillFilter } from 'react-icons/ai';
 
 export default function Aside() {
 
   return (
     <div className = {styles.container}>
-      <Image src = {Icon} alt = "Program icon" />
-
-      <div className = {styles.links}>
-          <Link href = "/">
-            <a> Sobre nós </a>
-          </Link>
-          <button className = "colored-btn">
-              Entrar
-          </button>
+      <div className = {styles.logoContainer}>
+        <Image src = {Icon} alt = "Program icon" />
       </div>
+
+      <button className = {styles.selected}>
+        <AiFillHome/> Dashboard
+      </button>
+
+      <p className = "larger"> Funções </p>
+      <button>
+        <AiOutlineUpload/> Upload
+      </button>
+      <button>
+        <AiFillFilter/> Filtragem
+      </button>
     </div>
   )
 }
--- nextjs/pages/flow.tsx
+++ nextjs/pages/flow.tsx
@@ -2,14 +2,17 @@ import Head from 'next/head'
 import styles from '../styles/Flow.module.css'
 
 import Header from '../components/Flow/Header'
-import Main from '../components/Home/Main'
+import Aside from '../components/Flow/Aside'
 import Footer from '../components/Home/Footer'
 
 export default function Flow() {
   return (
     <div className={styles.container}>
       <Head> <title> teOrienta | Fluxo </title> </Head>
-      <Header/>
+      <Aside/>
+      <div className = {styles.main}>
+        <Header/>
+      </div>
     </div>
   )
 }
